b93c648429573210101567ddd2c5b219fc76f991
FighT <33201145+NunoFighT@users.noreply.github.com>
2020-10-19 19:31:24 +0100

    HealthScheduler

---
+++ app/src/main/java/nunofernandes/example/healthscheduler/Login.kt
@@ -0,0 +1,28 @@
+package nunofernandes.example.healthscheduler
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        window.decorView.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+    }
+}
