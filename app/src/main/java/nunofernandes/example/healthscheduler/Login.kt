55b7e15fc9a55311bd70b4b61df42cc407cf61f5
NunoFighT8 <a18058@alunos.ipca.pt>
2020-10-19 20:14:53 +0100

    HealthScheduler

--- app/src/main/java/nunofernandes/example/healthscheduler/Login.kt
+++ app/src/main/java/nunofernandes/example/healthscheduler/Login.kt
@@ -1,20 +1,17 @@
 package nunofernandes.example.healthscheduler
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton = findViewById<Button>(R.id.buttonLogin)
+        val loginButton = findViewById<Button>(R.id.buttonLogin_Login)
 
         loginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
