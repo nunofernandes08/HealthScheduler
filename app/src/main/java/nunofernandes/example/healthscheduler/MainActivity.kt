310da9c2898757e72e4c299d1463e6f04b884be6
NunoFighT8 <a18058@alunos.ipca.pt>
2020-10-20 12:37:18 +0100

    HealthSheduler  - Adicionei um botao logout para teste Commit by Nuno

--- app/src/main/java/nunofernandes/example/healthscheduler/MainActivity.kt
+++ app/src/main/java/nunofernandes/example/healthscheduler/MainActivity.kt
@@ -1,11 +1,25 @@
 package nunofernandes.example.healthscheduler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btLogout = findViewById<Button>(R.id.btlogout)
+
+        btLogout.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            FirebaseAuth.getInstance().signOut()
+            startActivity(intent)
+        }
     }
 }
