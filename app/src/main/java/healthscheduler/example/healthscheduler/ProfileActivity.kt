2a2f4911a06f19774beb1eb5a32459ef22781543
Nuno Azevedo <ebm4562@live.com>
2020-11-30 15:32:04 +0000

    O editar nao esta a 100%

--- app/src/main/java/healthscheduler/example/healthscheduler/ProfileActivity.kt
+++ app/src/main/java/healthscheduler/example/healthscheduler/ProfileActivity.kt
@@ -1,21 +1,25 @@
 package healthscheduler.example.healthscheduler
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import com.google.gson.TypeAdapterFactory
 import com.squareup.picasso.Picasso
@@ -25,7 +29,7 @@ import healthscheduler.example.healthscheduler.databinding.ActivityProfileBindin
 import healthscheduler.example.healthscheduler.databinding.ActivityProfileV2Binding
 import healthscheduler.example.healthscheduler.models.MessageItem
 import healthscheduler.example.healthscheduler.models.UsersItem
-import java.util.HashMap
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -35,20 +39,123 @@ class ProfileActivity : AppCompatActivity() {
     private val auth        = Firebase.auth
     private val currentUser = auth.currentUser
 
+    private var currentUserName:    String?         = null
+    private var currentUserAddress: String?         = null
+    private var downUrl:            String?         = null
+    private var curFile:            Uri?            = null
+    private var user:               UsersItem?      = null
+
+    private lateinit var myDialog:  Dialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //val binding = ActivityProfileBinding.inflate(layoutInflater)
-        val binding = ActivityProfileV2Binding.inflate(layoutInflater)
+        val binding = ActivityProfileBinding.inflate(layoutInflater)
+        //val binding = ActivityProfileV2Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        getUser()
+        buttonsActions(binding)
         userInformation(binding)
         styleTextView(binding)
         backToHome(binding)
         logout(binding)
     }
 
-    private fun userInformation(/*binding: ActivityProfileBinding*/ binding: ActivityProfileV2Binding){
+    //Funcao para ir buscar a informacao do CURRENTUSER
+    private fun getUser() {
+        db.collection("users").document(currentUser!!.uid)
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                querySnapshot?.data?.let {
+                    user = UsersItem.fromHash(querySnapshot.data as HashMap<String, Any?>)
+                    user?.let { user ->
+                        currentUserName = user.username.toString()
+                        currentUserAddress = user.address.toString()
+                    }
+                } ?: run {
+                    Toast.makeText(
+                            this, "Sem utilizador",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    //Funcao com as acoes dos botoes
+    private fun buttonsActions(binding: ActivityProfileBinding){
+        binding.buttonEditProfile.setOnClickListener {
+            myDialog = Dialog(this)
+            myDialog.setContentView(R.layout.popwindow_edit)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+
+            //Botao para escolher foto
+            myDialog.findViewById<ImageView>(R.id.imageViewUserPhotoEdit).setOnClickListener {
+                Intent(Intent.ACTION_GET_CONTENT).also {
+                    it.type = "image/*"
+                    startActivityForResult(it, Home.REQUEST_CODE_IMAGE_PICK)
+                }
+            }
+
+            //Botao para submeter a atualizacao
+            myDialog.findViewById<Button>(R.id.buttonEditarEdit).setOnClickListener {
+                uploadImageToFirebaseStorage()
+                updateUser()
+            }
+            myDialog.show()
+        }
+    }
+
+    //Funcao para fazer upload da imagem para o FireStorage
+    private fun uploadImageToFirebaseStorage() {
+
+        val filename    = UUID.randomUUID().toString()
+        val ref         = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        curFile?.let{
+            ref.putFile(curFile!!)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+                        downUrl = it.toString()
+                    }
+                }
+                .addOnFailureListener {
+
+                }
+        }
+    }
+
+    //Funcao para fazer update do utilizador
+    private fun updateUser(){
+
+        val address = myDialog.findViewById<EditText>(R.id.editTextUserAddressEdit)
+
+        if(address.text.toString() == "") {
+            val user = UsersItem(currentUserName, currentUser!!.email, currentUserAddress, downUrl, currentUser.uid)
+            db.collection("users").document(currentUser.uid)
+                    .set(user.toHashMap())
+                    .addOnSuccessListener {
+                        Log.d("writeBD", "DocumentSnapshot successfully written!")
+                        myDialog.dismiss()
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("writeBD", "Error writing document", e)
+                    }
+        } else {
+            val user = UsersItem(currentUserName, currentUser!!.email, address.text.toString(), downUrl, currentUser.uid)
+            db.collection("users").document(currentUser.uid)
+                    .set(user.toHashMap())
+                    .addOnSuccessListener {
+                        Log.d("writeBD", "DocumentSnapshot successfully written!")
+                        myDialog.dismiss()
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("writeBD", "Error writing document", e)
+                    }
+        }
+    }
+
+    //Funcao que da informacao do CURRENTUSER
+    private fun userInformation(binding: ActivityProfileBinding /*binding: ActivityProfileV2Binding*/){
         currentUser?.uid.let {
             if (it != null) {
                 db.collection("users").document(it)
@@ -72,7 +179,8 @@ class ProfileActivity : AppCompatActivity() {
         }
     }
 
-    private fun styleTextView (/*binding: ActivityProfileBinding*/ binding: ActivityProfileV2Binding){
+    //Funcao com estilo das textViews
+    private fun styleTextView (binding: ActivityProfileBinding /*binding: ActivityProfileV2Binding*/){
         binding.textViewInformacaoConta.setTypeface(null, Typeface.BOLD)
         binding.textViewUserNameNomeProfile.setTypeface(null, Typeface.BOLD)
         binding.textViewUserPhoneProfile.setTypeface(null, Typeface.BOLD)
@@ -80,14 +188,16 @@ class ProfileActivity : AppCompatActivity() {
         binding.textViewUserBirthdayProfile.setTypeface(null, Typeface.BOLD)
     }
 
-    private fun backToHome(binding: ActivityProfileV2Binding){
+    //Funcao que volta para o home
+    private fun backToHome(binding: ActivityProfileBinding /*binding: ActivityProfileV2Binding*/){
         binding.floatingActionButton.setOnClickListener {
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
         }
     }
 
-    private fun logout(binding: ActivityProfileV2Binding){
+    //Funcao para dar logout
+    private fun logout(binding: ActivityProfileBinding /*binding: ActivityProfileV2Binding*/){
         binding.buttonLogoutProfile.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -100,6 +210,23 @@ class ProfileActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             FirebaseAuth.getInstance().signOut()
             startActivity(intent)
+        }
+    }
+
+    //Abrir a janela para escolher a foto
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val imageViewUserPhotoEdit = findViewById<ImageView>(R.id.imageViewUserPhotoEdit)
+
+        if (resultCode === Activity.RESULT_OK) {
+            if (requestCode == Home.REQUEST_CODE_IMAGE_PICK) {
+                data?.data?.let {
+
+                    curFile = it
+                    myDialog.findViewById<ImageView>(R.id.imageViewUserPhotoEdit).setImageURI(curFile)
+                }
+            }
         }
     }
 }
