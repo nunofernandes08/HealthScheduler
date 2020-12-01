8ed8499eeb457d24cd27a9196f328d76c77c2a40
Nuno Azevedo <ebm4562@live.com>
2020-12-01 17:17:05 +0000

    Comit teste RowSchedule

--- app/src/main/java/healthscheduler/example/healthscheduler/Schedule.kt
+++ app/src/main/java/healthscheduler/example/healthscheduler/Schedule.kt
@@ -1,37 +1,39 @@
 package healthscheduler.example.healthscheduler
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
-import healthscheduler.example.healthscheduler.databinding.ActivityHomeBinding
-import healthscheduler.example.healthscheduler.databinding.ActivityProfileBinding
 import healthscheduler.example.healthscheduler.databinding.ActivityScheduleBinding
 import healthscheduler.example.healthscheduler.models.ScheduleItem
 import healthscheduler.example.healthscheduler.models.UsersItem
 import kotlinx.android.synthetic.main.activity_schedule.*
-import kotlinx.android.synthetic.main.row_schedule.*
+import kotlinx.android.synthetic.main.popwindow_schedule_detail.*
 import java.util.ArrayList
 import java.util.HashMap
 
 class Schedule : AppCompatActivity() {
+
+    private val db          = FirebaseFirestore.getInstance()
+    private val auth        = Firebase.auth
+    private val currentUser = auth.currentUser
 
     private var listSchedule:       MutableList<ScheduleItem> = ArrayList()
     private var scheduleAdapter:    Schedule.ScheduleAdapter? = null
 
     private var listUser:           UsersItem? = null
 
-    private val db          = FirebaseFirestore.getInstance()
-    private val auth        = Firebase.auth
-    private val currentUser = auth.currentUser
+    private lateinit var myDialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,6 +66,9 @@ class Schedule : AppCompatActivity() {
                                 document.data.getValue("doctorName").toString(),
                                 document.data.getValue("hour").toString(),
                                 document.data.getValue("local").toString(),
+                                document.data.getValue("floor").toString(),
+                                document.data.getValue("pavilion").toString(),
+                                document.data.getValue("cabinet").toString(),
                                 document.data.getValue("typeOfConsult").toString()))
                     }
                     scheduleAdapter?.notifyDataSetChanged()
@@ -106,7 +111,7 @@ class Schedule : AppCompatActivity() {
     //Adapter das consultas
     inner class ScheduleAdapter : BaseAdapter() {
         override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val rowView = layoutInflater.inflate(R.layout.row_schedule, parent, false)
+            val rowView = layoutInflater.inflate(R.layout.row_schedule_v2, parent, false)
 
             val textViewDoctorNameSchedule = rowView.findViewById<TextView>(R.id.textViewDoctorNameSchedule)
             val textViewLocationSchedule = rowView.findViewById<TextView>(R.id.textViewLocationSchedule)
@@ -119,6 +124,16 @@ class Schedule : AppCompatActivity() {
             textViewHourSchedule.text = listSchedule[position].hour
             textViewLocationSchedule.text = listSchedule[position].local
             textViewTypeOfConsultSchedule.text = listSchedule[position].typeOfConsult
+
+            rowView.setOnClickListener {
+                myDialog = Dialog(this@Schedule)
+                    myDialog.setContentView(R.layout.popwindow_schedule_detail)
+                    myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+
+                    myDialog.textViewTypeOfConsultDetailV2.text = listSchedule[position].typeOfConsult
+
+                myDialog.show()
+            }
 
             return rowView
         }
--- app/src/main/java/healthscheduler/example/healthscheduler/models/ScheduleItem.kt
+++ app/src/main/java/healthscheduler/example/healthscheduler/models/ScheduleItem.kt
@@ -3,11 +3,14 @@ package healthscheduler.example.healthscheduler.models
 import kotlin.collections.HashMap
 
 class ScheduleItem {
-    var date: String? = null
-    var doctorName: String? = null
-    var hour: String? = null
-    var local: String? = null
-    var typeOfConsult: String? = null
+    var date:           String? = null
+    var doctorName:     String? = null
+    var hour:           String? = null
+    var local:          String? = null
+    var floor:          String? = null
+    var pavilion:       String? = null
+    var cabinet:        String? = null
+    var typeOfConsult:  String? = null
 
 
     constructor(
@@ -15,12 +18,18 @@ class ScheduleItem {
             doctorName: String?,
             hour: String?,
             local: String?,
+            floor: String?,
+            pavilion: String?,
+            cabinet: String?,
             typeOfConsult: String?
     ) {
         this.date = date
         this.doctorName = doctorName
         this.hour = hour
         this.local = local
+        this.floor = floor
+        this.pavilion = pavilion
+        this.cabinet = cabinet
         this.typeOfConsult = typeOfConsult
     }
 }
